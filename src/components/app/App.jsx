aac4ccc5541ed1c36d7e926fe2b9cc91cffcd14f
Stark <serj.elessar@gmail.com>
2023-06-22 21:21:05 +0300

    fix

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,71 +1,73 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import axios from "axios";
+import Countries from '../countries/Countries';
+import CountriesInfo from '../countriesInfo/CountriesInfo';
 
-import Countries from "../countries/Countries";
-import CountriesInfo from "../countriesInfo/CountriesInfo";
+import './App.css';
 
-import "./App.css";
+const URL_API = 'https://restcountries.com/v3.1/all';
 
 function App() {
-  const [countries, setCountries] = useState([]);
-  const [activeIdCountry, setActiveIdCountry] = useState(464);
-  const [countryInfo, setCountryInfo] = useState(null);
-  const [loading, setLoading] = useState(true);
+	const [countries, setCountries] = useState([]);
+	const [activeIdCountry, setActiveIdCountry] = useState(null);
+	const [activeCountry, setActiveCountry] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-  function fetchCountries() {
-    const URL_API = "https://restcountries.com/v3.1/all";
-    axios.get(URL_API).then(({ data }) => {
-      setCountries(data);
-      setLoading(false);
-    });
-  }
+	const getActiveCountry = (countries, id) => {
+		const [activeCountry] = countries.filter(({ area }) => area === id);
 
-  useEffect(() => {
-    fetchCountries();
-  }, [activeIdCountry]);
+		return activeCountry;
+	};
 
-  const handleClick = (id) => {
-    setCountryInfo(id);
-    setActiveIdCountry(id);
-    const currentCountry = countries.filter(
-      ({ area }) => area === activeIdCountry
-    );
-    console.log(currentCountry[0]);
-    setCountryInfo(currentCountry);
-  };
+	const fetchCountries = async () => {
+		const { data } = await axios.get(URL_API);
+		setCountries(data);
+		setActiveCountry(getActiveCountry(data, data[0].area));
+		setActiveIdCountry(data[0].area);
+		setLoading(false);
+	}
 
-  return (
-    <div className="app">
-      <h1>Countries</h1>
-      <div className="app__container countries">
-        <div className="countries__body">
-          {/* {countries.length && (
-            <>
-              <Countries
-                countries={countries}
-                handleClick={handleClick}
-                activeIdCountry={activeIdCountry}
-              />
-              <CountriesInfo countryInfo={countryInfo} />
-            </>
-          )} */}
-          {loading ? (
-            <span>Loading...</span>
-          ) : (
-            <>
-              <Countries
-                countries={countries}
-                handleClick={handleClick}
-                activeIdCountry={activeIdCountry}
-              />
-              <CountriesInfo countryInfo={countryInfo} />
-            </>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		fetchCountries();
+	}, []);
+
+	const handleClick = (id) => {
+		setActiveIdCountry(id);
+		setActiveCountry(getActiveCountry(countries, id));
+	};
+
+	return (
+		<div className="app">
+			<h1>Countries</h1>
+			<div className="app__container countries">
+				<div className="countries__body">
+					{/* {countries.length && (
+					 <>
+					 <Countries
+					 countries={countries}
+					 handleClick={handleClick}
+					 activeIdCountry={activeIdCountry}
+					 />
+					 <CountriesInfo countryInfo={countryInfo} />
+					 </>
+					 )} */}
+					{loading ? (
+						<span>Loading...</span>
+					) : (
+						<>
+							<Countries
+								countries={countries}
+								handleClick={handleClick}
+								activeIdCountry={activeIdCountry}
+							/>
+							<CountriesInfo activeCountry={activeCountry}/>
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/countriesInfo/CountriesInfo.jsx
+++ src/components/countriesInfo/CountriesInfo.jsx
@@ -1,40 +1,25 @@
-import "./countriesInfo.css";
+import './countriesInfo.css';
 
-const InfoCountry = ({ countryInfo }) => {
-  // console.log(countryInfo);
-  return (
-    <>
-      {/* <div>Текущая страна: {countryInfo.name?.common}</div> */}
-      {/* <div className="info">
-        <div className="info__container">
-          <span className="info__flag">{countryInfo.flags?.png}</span>
-          <div className="info-up">
-            <h2 className="info__name">{countryInfo.name?.common}</h2>
-            <h3 className="info__capital">{countryInfo.capital?.[0]}</h3>
-          </div>
-          <div className="info-down">
-            <span>{countryInfo.region}</span>
-            <span>{countryInfo.timezones?.[0]}</span>
-            <span>{countryInfo.languages?.[0]}</span>
-          </div>
-        </div>
-      </div> */}
-      <div className="countries__info">
-        <div className="countries__info-container">
-          <span className="countries__info-flag"></span>
-          <div className="countries__info-up">
-            <h2 className="countries__info__name">asd</h2>
-            <h3 className="countries__info__capital">asdasdas</h3>
-          </div>
-          <div className="countries__info-down">
-            <span>asdasd</span>
-            <span>asdasdas</span>
-            <span>asdasdaa</span>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+const InfoCountry = ({ activeCountry: { flags, altSpellings, name, region, timezones, languages} }) => {
+
+	return (
+		<div className="info">
+			<div className="info__container">
+				<div className="info__flag">
+					<img src={flags.png} alt={altSpellings[1]}/>
+				</div>
+				<div className="info-up">
+					<h2 className="info__name">{name.common}</h2>
+					<h3 className="info__capital">{name.official}</h3>
+				</div>
+				<div className="info-down">
+					<span>{region}</span>
+					<span>{timezones[0]}</span>
+					<span>{languages[0]}</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default InfoCountry;
