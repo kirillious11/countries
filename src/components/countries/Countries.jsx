27c551dbb0d7ba64066f1fe3a2ebd84ba5523d5e
Kirill <ks_20_11@mail.ru>
2023-07-18 23:53:16 +0300

    add country not found

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,27 +1,32 @@
 import "./countries.css";
 
 const Countries = ({ countries, handleClick, activeIdCountry }) => {
+  console.log(countries);
   return (
     <>
       <ul className="countries__list">
-        {countries.map(({ area, flags: { png, alt }, name }, i) => {
-          const key = `${area}_${i}`;
+        {countries.length < 1 ? (
+          <span>Countries not found</span>
+        ) : (
+          countries.map(({ area, flags: { png, alt }, name }, i) => {
+            const key = `${area}_${i}`;
 
-          return (
-            <li
-              onClick={() => handleClick(area)}
-              className={`country__item ${
-                activeIdCountry === area ? "active" : ""
-              }`}
-              key={key}
-            >
-              <img src={png} alt={alt} style={{ width: 50 }} />
-              <span>
-                {name.common} {area}
-              </span>
-            </li>
-          );
-        })}
+            return (
+              <li
+                onClick={() => handleClick(area)}
+                className={`country__item ${
+                  activeIdCountry === area ? "active" : ""
+                }`}
+                key={key}
+              >
+                <img src={png} alt={alt} style={{ width: 50 }} />
+                <span>
+                  {name.common} {area}
+                </span>
+              </li>
+            );
+          })
+        )}
       </ul>
     </>
   );
